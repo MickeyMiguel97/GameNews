9d4956137e911821d168f5aefdfe85bac843c9ee
Miguel Yanes <00145016@uca.edu.sv>
2018-06-16 16:28:06 -0600

    Pequeñas modificaciones del room

--- app/src/main/java/com/example/yanirayanes/gamenews/BaseDeDatos/RoomDB.java
+++ app/src/main/java/com/example/yanirayanes/gamenews/BaseDeDatos/RoomDB.java
@@ -1,11 +1,15 @@
 package com.example.yanirayanes.gamenews.BaseDeDatos;
 
 import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
+import com.example.yanirayanes.gamenews.PlainOldJavaObjects.Noticia;
+
+@Database(entities = {Noticia.class}, version = 1)
 public abstract class RoomDB extends RoomDatabase{
     public abstract NoticiasDAO noticiaDAO();
     private static RoomDB INSTANCE;
@@ -23,11 +27,4 @@ public abstract class RoomDB extends RoomDatabase{
         }
         return INSTANCE;
     }
-    private static RoomDatabase.Callback rdbRoomDatabaseCallback = new RoomDatabase.Callback(){
-        @Override
-        public void onOpen(@NonNull SupportSQLiteDatabase db) {
-            super.onOpen(db);
-        }
-    };
-
 }
