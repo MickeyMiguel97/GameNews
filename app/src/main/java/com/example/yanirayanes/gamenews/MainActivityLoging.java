95c1f29274e31cb19ac68187f6e72b9737cf4d76
Miguel Yanes <00145016@uca.edu.sv>
2018-06-16 18:13:24 -0600

    Login ya hecho :D

--- app/src/main/java/com/example/yanirayanes/gamenews/MainActivityLoging.java
+++ app/src/main/java/com/example/yanirayanes/gamenews/MainActivityLoging.java
@@ -1,5 +1,9 @@
 package com.example.yanirayanes.gamenews;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,31 +13,41 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.yanirayanes.gamenews.PlainOldJavaObjects.Token;
+import com.example.yanirayanes.gamenews.TodoLoDeLaAPI.ApiEndPoint;
+import com.example.yanirayanes.gamenews.TodoLoDeLaAPI.NoticiasJuegosAPI;
+
+import okhttp3.internal.Util;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivityLoging extends AppCompatActivity {
 
     Button BotonSignIn;
-    //NoticiasJuegosAPI NoticiasJuegosApi;
-    String EnviarToken;
+    EditText EditTextPassword,EditTextUser;
+    NoticiasJuegosAPI api;
+    Token post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_loging);
 
+        post = new Token();
+        EditTextUser = findViewById(R.id.EditTextUser);
+        EditTextPassword = findViewById(R.id.EditTextPassword);
         BotonSignIn = (Button) findViewById(R.id.BotonSignIn);
 
-        //NoticiasJuegosApi = ApiUtiles.getNoticiasJuegosAPI();
+        api = ApiEndPoint.getGameNewsAPI();
 
         BotonSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final EditText EditTextUser = (EditText) findViewById(R.id.EditTextUser);
-                final EditText EditTextPassword = (EditText) findViewById(R.id.EditTextPassword);
-
                 String User = EditTextUser.getText().toString().trim();
                 String Password = EditTextPassword.getText().toString().trim();
-
                 if (!TextUtils.isEmpty(User) && !TextUtils.isEmpty(Password)) {
                     MandarToken(User, Password);
                 }
@@ -42,18 +56,30 @@ public class MainActivityLoging extends AppCompatActivity {
     }
 
     public void MandarToken(final String User, final String Password){
+        api.savePost(User,Password).enqueue(new Callback<Token>() {
+            @Override
+            public void onResponse(Call<Token> call, Response<Token> response) {
+                if(response.isSuccessful()){
+                    showResponse(response.body().toString());
+                    if(response.body().toString() != null){
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+                    }else {
+                        Toast.makeText(getApplicationContext(),"Usuario no registrado", Toast.LENGTH_SHORT).show();
+                    }
+                    Log.i("NoEntendíx3","El token se mando a la api"+ response.body().toString());
+                }
 
+            }
+
+            @Override
+            public void onFailure(Call<Token> call, Throwable t) {
+                Log.e("NoEntendíx2","No se pudo mandar el token a la Api");
+            }
+        });
     }
 
-    private String guardarToken(Token token) {
-        SharedPreferences preferencias = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor prefEditor = preferencias.edit();
-        prefEditor.putString("tokenpdm", token.getToken());
-        prefEditor.commit();
-
-        Log.i("PASE", "TOKEN: "+preferencias.getString("tokenpdm", "noToken"));
-
-        return token.getToken();
-
+    public void showResponse(String response) {
+        Log.d("NoEntendi", "es null: "+(response==null));
     }
 }
